1a32e03f31010e8d7bcf9bbe5c3dc21499c729e3
p22cobrien <p22cobrien@fairfieldprep.org>
2021-10-27 09:40:51 -0400

    add hw5-2.py

---
+++ hw5-2.py
@@ -0,0 +1,32 @@
+# Author: CMOB 10/26/2021
+import math
+
+# 1) Write a line of code that returns square root of 529.
+print(math.sqrt(529))
+
+# 2) Write a line of code that returns the sine of 0.
+print(math.sin(0))
+
+# 3) Write a line of code that returns the cosine of 2 pi.
+print(math.cos(2 * math.pi))
+
+# 4) Write a line of code that returns 2.5 raised to the power of 5.
+print(math.pow(2.5, 5))
+
+# 5) Write a line of code that returns the natural log of e.
+print(math.log(math.e))
+
+# 6) Write a line of code that returns e raised to the power of 4.
+print(math.pow(math.e, 4))
+
+# 7) Write a line of code that returns the ceiling of -12.9.
+print(math.ceil(-12.9))
+
+# 8) Write a line of code that returns the floor of 7.25.
+print(math.floor(7.25))
+
+# 9) Write a line of code that returns the natural log of 1000, times 2.
+print(math.log(math.log(1000)))
+
+# 10) Write a line of code that converts 180 degrees to radians.
+print(math.radians(180))
